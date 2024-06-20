4a1a2d593fe139f5127f50a9200ac11630b7f585
Gurpreet Singh <gurpreet.singh@crownit.in>
2024-06-20 06:45:53 +0530

    Added jenkins and terraform scripts

---
+++ terraform/ec2_instance.tf
@@ -0,0 +1,43 @@
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_key_pair" "ssh_key" {
+  key_name   = "jenkins_key"
+  public_key = file("id_ed25519.pub")
+}
+
+resource "aws_instance" "test" {
+  ami = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  associate_public_ip_address = true
+  tags = {
+    Name = "test_server"
+  }
+  user_data = file("apache.sh")
+  key_name = "ap-south"
+}
+
+resource "aws_instance" "prod" {
+  ami = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  associate_public_ip_address = true
+  tags = {
+    Name = "prod_server"
+  }
+  user_data = file("apache.sh")
+  //key_name = aws_key_pair.ssh_key.key_name
+  key_name = "ap-south"
+}
